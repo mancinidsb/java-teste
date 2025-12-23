cc7090d7f781341a6723c7daf24fda78c5f851df
mancinidsb <guilhermemancinidsb@gmail.com>
2025-12-23 20:17:38 -0300

    Update maven-ci and remove spaces in Animal.java

--- src/main/java/com/github/mancinidsb/javateste/models/Animal.java
+++ src/main/java/com/github/mancinidsb/javateste/models/Animal.java
@@ -9,31 +9,21 @@ public class Animal {
 		this.peso=peso;
 	}
 	
-	
-	
 	public String getNome() {
 		return nome;
 	}
-
-
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
-
 	public double getPeso() {
 		return peso;
 	}
 
-
-
 	public void setPeso(double peso) {
 		this.peso = peso;
 	}
-
-
 
 	public String toString() {
 		return "Nome: "+nome+"\nPeso: "+peso;
